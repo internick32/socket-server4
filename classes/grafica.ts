182ea4fa3c79c02c57a6e797fc177b6681fc8408
Trigalo27 <internick27@gmail.com>
2021-02-23 12:45:36 -0600

    commit a socket-server4

---
+++ classes/grafica.ts
@@ -0,0 +1,27 @@
+
+export class GraficaData {
+
+    private labels: string[] = [];
+    private valores: number[] = [0, 0, 0, 0];
+
+    constructor() { }
+
+    setLabels( labels: string[] ) {
+        this.labels = labels;
+    }
+
+    getDataGrafica() {
+        return [
+            { data: this.valores, label: 'Preguntas' }
+        ];
+    }
+
+
+    incrementarValor( opcion: number, valor: number ) {
+
+        this.valores[opcion] += valor;
+        return this.getDataGrafica();
+
+    }
+
+}
